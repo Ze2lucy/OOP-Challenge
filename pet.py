19f1732602d1e4b75e4581fc5f99f3ba94f7544f
Ze2-lucy <zethulucysitole@gmail.com>
2025-04-21 19:42:55 +0200

    add get_toys_by_type and eat methods to Pet class

--- pet.py
+++ pet.py
@@ -8,3 +8,17 @@ class Pet:
         self.happiness = happiness
         self.tricks = []
         self.toys = self.get_toys_by_type()
+
+    def get_toys_by_type(self):
+        petToys = {
+            "cat": ["scratching post", "toy mouse"],
+            "dog": ["chew bone", "frisbee"],
+            "fish": ["underSea castle", "net", "buried treasure"]
+        }
+        return petToys.get(self.type, [])
+
+    def eat(self):
+        self.hunger = max(0, self.hunger - 3)
+        self.happiness = min(10, self.happiness + 1)
+        print(
+            f"{self.name} just ate! Hunger: {self.hunger}, Happiness: {self.happiness}")
